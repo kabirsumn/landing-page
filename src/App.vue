c76f94add2a104eb0591e04f17199e3966cd5ad1
kabirsumn <kabirinnow26@gmail.com>
2022-01-25 23:41:19 +0600

    form component created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>welcome</h1>
+  <form />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Form },
+};
 </script>
 
 <style>
---
+++ src/components/Form.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="form">
+    <form action="post">
+      <label for="username">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="username"
+          required
+        />
+      </label>
+      <label for="password">
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="password"
+          required
+        />
+      </label>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
